740297a475b4c436b282311bd92f778693e42241
swehio <63448248+swehio@users.noreply.github.com>
2025-04-22 09:27:47 +0900

    [level 1] Title: 공원 산책, Time: 0.07 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
@@ -1,60 +1,60 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(vector<string> park, vector<string> routes) {
-    int h = park.size();           // 세로 길이
-    int w = park[0].size();        // 가로 길이
-    int x = 0, y = 0;              // 시작 위치
-
-    // 시작 위치 찾기
-    for (int i = 0; i < h; ++i) {
-        for (int j = 0; j < w; ++j) {
-            if (park[i][j] == 'S') {
-                x = i;
-                y = j;
-                break;
+    vector<int> answer;
+    vector<int> currentPosition(2);
+    unordered_map<char, pair<int, int>> directionMap;
+    
+    for(int i = 0; i<park.size(); ++i)
+    {
+        for(int j = 0; j < park[i].size(); ++j)
+        {
+            if(park[i][j] == 'S')
+            {
+                currentPosition[0] = i;
+                currentPosition[1] = j;
             }
         }
     }
-
-    // 방향 정의
-    int dx[128] = {0}, dy[128] = {0};
-    dx['N'] = -1; dy['N'] = 0;
-    dx['S'] = 1;  dy['S'] = 0;
-    dx['W'] = 0;  dy['W'] = -1;
-    dx['E'] = 0;  dy['E'] = 1;
-
-    // 명령 처리
-    for (const string& route : routes) {
-        char dir;
-        int dist;
+    
+    directionMap['E'] = {0, 1};
+    directionMap['S'] = {1, 0};
+    directionMap['W'] = {0, -1};
+    directionMap['N'] = {-1, 0};
+    
+    for(string route : routes)
+    {
         stringstream ss(route);
-        ss >> dir >> dist;
-
-        int nx = x;
-        int ny = y;
-        bool canMove = true;
-
-        for (int i = 0; i < dist; ++i) {
-            nx += dx[dir];
-            ny += dy[dir];
-
-            // 범위 벗어나거나 장애물 만나면 이동 불가
-            if (nx < 0 || ny < 0 || nx >= h || ny >= w || park[nx][ny] == 'X') {
-                canMove = false;
+        char direction;
+        int num;
+        ss >> direction >> num;
+        
+        vector<int> tempCurrentPosition(currentPosition.begin(), currentPosition.end());
+        
+        for(int i = 0; i<num; ++i)
+        {
+            vector<int> newPosition(2);
+            newPosition[0] = currentPosition[0] + directionMap[direction].first;
+            newPosition[1] = currentPosition[1] + directionMap[direction].second;
+            bool verticalInRange = newPosition[0] >= 0 && newPosition[0] < park.size();
+            bool HorizonInRange = newPosition[1] >= 0 && newPosition[1] < park[1].size();
+            if(!verticalInRange || !HorizonInRange || park[newPosition[0]][newPosition[1]] == 'X')
+            {
+                currentPosition[0] = tempCurrentPosition[0];
+                currentPosition[1] = tempCurrentPosition[1];
                 break;
             }
-        }
-
-        // 이동 가능하면 좌표 갱신
-        if (canMove) {
-            x = nx;
-            y = ny;
+            currentPosition[0] = newPosition[0];
+            currentPosition[1] = newPosition[1];
         }
     }
-
-    return {x, y};
+    answer.push_back(currentPosition[0]);
+    answer.push_back(currentPosition[1]);
+    return answer;
 }
