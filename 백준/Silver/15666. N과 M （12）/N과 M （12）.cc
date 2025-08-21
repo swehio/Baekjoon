72a1e46fd67ae60b1d4ee93077c2f4210bfeac7c
한가윤 <63448248+swehio@users.noreply.github.com>
2025-08-21 09:17:36 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, st;
+int arr[10];
+vector<int> num;
+
+void solve(int k)
+{
+    if(k == M)
+    {
+        for(int i = 0; i<M; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    int temp = 0;
+    
+    if(k == 0) st = 0;
+    for(int i = st; i < N; i++)
+    {
+        if(temp != num[i])
+        {
+            arr[k] = num[i];
+            temp = num[i];
+            st = i;
+            solve(k+1);
+        }
+    }
+    
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N >> M;
+    
+    for(int i = 0; i<N; i++)
+    {
+        int input;
+        cin >> input;
+        num.push_back(input);
+    }
+    
+    sort(num.begin(), num.end());
+    solve(0);
+}
