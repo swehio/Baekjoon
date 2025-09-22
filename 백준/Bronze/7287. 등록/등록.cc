2a61c2357bdd0c924c4ceadc20242f4997c7ef9a
한가윤 <63448248+swehio@users.noreply.github.com>
2025-09-22 16:00:15 +0900

    [Bronze V] Title: 등록, Time: 8 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string sortStudent[500002];
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "141" << '\n';
+    cout << "swehio7219" << '\n';
+}
