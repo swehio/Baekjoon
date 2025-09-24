59f4e3422d8f91bae48b37936bcf65a6385dc8dc
한가윤 <63448248+swehio@users.noreply.github.com>
2025-09-24 23:27:36 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr1[102][102];
+int arr2[102][102];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    double a, b;
+    cin >> a >> b;
+    cout.precision(10);
+
+    cout << a/b;
+}   
