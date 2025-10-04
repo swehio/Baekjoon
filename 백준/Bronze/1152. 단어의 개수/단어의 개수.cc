1b6b5254c02de49262b2b23d16ea1f67e1375691
한가윤 <63448248+swehio@users.noreply.github.com>
2025-10-04 13:32:25 +0900

    [Bronze II] Title: 단어의 개수, Time: 24 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int cnt = 0;
+
+    while(1)
+    {
+        string word;
+        cin >> word;
+        if(cin.eof()) break;
+
+        cnt++;
+    }
+
+    cout << cnt;
+}
