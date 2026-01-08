225ea6245ebe93829135b8fdafe5cc838ea45ee5
한가윤 <63448248+swehio@users.noreply.github.com>
2026-01-08 10:25:07 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end(), [](char a, char b){ return a>b;});
+
+    cout << s;
+}
