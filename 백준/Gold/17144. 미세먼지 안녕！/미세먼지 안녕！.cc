316f4d7618b7f347102e416010be1fcbe63647dd
한가윤 <63448248+swehio@users.noreply.github.com>
2026-02-22 18:24:49 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 32 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -1,41 +1,42 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-
-int board[55][55];
-int tmp[55][55];
-int ans = 2;
-int airCleanerUp = -1;
-int airCleanerDown;
+ 
 int r, c, t;
+int board[55][55];
+int ans;
+int ac;
 
-int dx[] = {0, -1, 0, 1};
-int dy[] = {-1, 0, 1, 0};
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
 
 void spread()
 {
+    int tmp[55][55] = {0};
+
     for(int i=0; i<r; i++)
     {
         for(int j=0; j<c; j++)
         {
-            if(board[i][j] > 0)
+            if(board[i][j]>0)
             {
                 int cnt = 0;
-                int spreadAmount = board[i][j]/5;
+                int amount = ceil(board[i][j]/5);
                 for(int k=0; k<4; k++)
                 {
                     int nx = i+dx[k];
                     int ny = j+dy[k];
 
                     if(nx<0||nx>=r||ny<0||ny>=c) continue;
-                    if(board[nx][ny] == -1) continue;
-                     
-                    tmp[nx][ny] += spreadAmount;
+                    if(board[nx][ny]==-1) continue;
+
+                    tmp[nx][ny] += amount;
                     cnt++;
                 }
-                board[i][j] -= spreadAmount*cnt;
+                tmp[i][j] += -(amount*cnt);
             }
         }
-    }
+    } 
 
     for(int i=0; i<r; i++)
     {
@@ -46,67 +47,71 @@ void spread()
     }
 }
 
-void rotate(int stX, int dir)
+void rotateDust(int row, bool isclock)
 {
-    int cx = (dir==1)?stX-1:stX+1;
-    int cy = 0; 
-    int movedir = dir;
-    int t = 30; 
+    pair<int, int> cur = {row, 0};
+    int dir = isclock?2:0;
 
-    while(!(cx==stX&&cy==0))
+    int nx = cur.first;
+    int ny = cur.second;
+
+    int up=isclock?row-1:0;
+    int left=0;
+    int right=c;
+    int down=isclock?r:row+2;
+
+    while(board[nx][ny]!=-1)
     {
-        int nx=cx+dx[movedir];
-        int ny=cy+dy[movedir];
-        bool xmaxLimit = (dir==1) ? nx<0 : nx>=r;
-        bool xminLimit = (dir==1) ? nx>stX : nx<stX;
-        if(xminLimit||xmaxLimit||ny<0||ny>=c)
+        nx = cur.first+dx[dir];
+        ny = cur.second+dy[dir];
+ 
+        if(nx<up||nx>=down||ny<left||ny>=right)
         {
-            movedir = (dir==1)?(movedir+1)%4:(movedir-1)%4;
-            nx=cx+dx[movedir];
-            ny=cy+dy[movedir];
+            int add = (isclock?-1:1);
+            dir=(dir+add+4)%4; 
+            continue;
         }
-        if(board[nx][ny]!=-1) board[cx][cy] = board[nx][ny]; 
-        else board[cx][cy] = 0; 
-        cx = nx; cy = ny;
+
+        board[cur.first][cur.second] = (board[nx][ny] == -1)?0:board[nx][ny];
+        cur = {nx, ny};
     }
 }
+
+void performac()
+{
+    rotateDust(ac-1, false);
+    rotateDust(ac+2, true);
+}
+
 
 int main()
 {
     ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0); 
 
     cin >> r >> c >> t;
- 
+
     for(int i=0; i<r; i++)
     {
         for(int j=0; j<c; j++)
         {
             cin >> board[i][j];
-            if(board[i][j] == -1 && airCleanerUp == -1)
-            {
-                airCleanerUp = i;
-                airCleanerDown = i+1;
-            }
-        } 
+            if(board[i][j]==-1&&ac==0)
+                ac = i;
+        }
     }
 
     while(t--)
     {
-        for(int i=0; i<r; i++)
-        {
-            fill(tmp[i], tmp[i]+c, 0);
-        }
-        spread();  
-        rotate(airCleanerUp, 1);
-        rotate(airCleanerDown, 3); 
+        spread();
+        performac(); 
     }
 
     for(int i=0; i<r; i++)
     {
         for(int j=0; j<c; j++)
         {
-            ans+=board[i][j];
+            if(board[i][j]!=-1)ans+=board[i][j];
         }
     }
 
