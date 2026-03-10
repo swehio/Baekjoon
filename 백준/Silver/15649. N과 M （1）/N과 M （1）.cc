3043dda3afdaf01383dbf6db6dd2538cfcf4e54d
한가윤 <63448248+swehio@users.noreply.github.com>
2026-03-10 20:43:48 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,38 +1,35 @@
-#include <bits/stdc++.h>
-using namespace std; 
+#include<bits/stdc++.h>
+using namespace std;  
 
-int a[10];
-bool vis[10];
 int n, m;
+int a[10];
+bool used[10];
 
-void solve(int depth)
-{
-    if(depth == m)
+void solve(int k)
+{ 
+    if(k==m)
     {
-        for(int i=1; i<=m; i++)
-        {
-            cout << a[i] << ' ';
-        }
+        for(int i=0; i<m; i++) cout << a[i] << ' ';
         cout << '\n';
+        return;
     }
 
     for(int i=1; i<=n; i++)
     {
-        if(vis[i]) continue;
-        a[depth+1] = i;
-        vis[i] = 1;     
-        solve(depth+1);
-        vis[i] = 0;
+        if(used[i]) continue;
+        a[k] = i;
+        used[i]=true;
+        solve(k+1);
+        used[i]=false;
     }
 }
 
 int main()
 {
     ios::sync_with_stdio(0);
-    cin.tie(0); 
-
-    cin >> n >> m;
+    cin.tie(0);
+ 
+    cin>>n>>m;
 
     solve(0);
-
-}
+} 
